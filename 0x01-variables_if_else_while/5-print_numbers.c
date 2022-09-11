604ff3ee9876db3549125c044a316ac6c38bea6a
antonixe <khaaoyatoony@gmail.com>
2022-09-11 10:36:39 -0700

    print number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#include <time.h>
+
+#include <stdlib.h>
+
+/**
+ * main - main
+ * Description: prints all single digit
+ * Return: 0
+ */
+int main(void)
+{
+	char x = '0';
+
+	while (x <= '9')
+	{
+		putchar(x);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
+
+
