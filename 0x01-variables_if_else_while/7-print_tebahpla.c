4f5488363e871f09bb71b9510e1f61491558adee
antonixe <khaaoyatoony@gmail.com>
2022-09-11 11:45:43 -0700

    reverse alphabets

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#include <time.h>
+
+#include <stdlib.h>
+
+/**
+ * main - main
+ * Description: numbers of base 10
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c = 'z';
+
+	while (c >= 'a')
+	{
+		putchar(c);
+		c--;
+	}
+
+	putchar('\n');
+	return (0);
+}
