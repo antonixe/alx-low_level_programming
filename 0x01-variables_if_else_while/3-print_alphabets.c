3461c024524fc2356499f5e60bf1e60b3f4c4de4
antonixe <khaaoyatoony@gmail.com>
2022-09-11 10:04:28 -0700

    alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - letters
+ * Description: aphabet in lower
+ * Return: zero
+ */
+int main(void)
+{
+	int i, j;
+	j = 65;
+
+	while (i < 123)
+	{
+		putchar(i);
+		i++;
+	}
+
+	while (j < 91)
+	{
+		putchar(j);
+		j++;
+	}
+
+	putchar(10);
+	return (0);
+}
