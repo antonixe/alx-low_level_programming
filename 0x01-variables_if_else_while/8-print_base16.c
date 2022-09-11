c297fe3a82ff65a58a630c7c9be6ac12a9c8b102
antonixe <khaaoyatoony@gmail.com>
2022-09-11 11:51:14 -0700

    base numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main
+ * Description: single digit numbers of base 10
+ * Return: 0
+ */
+int main(void)
+{
+	char d = '0';
+
+	while (d <= '9')
+	{
+		putchar(d);
+		d++;
+	}
+
+	char c = 'a';
+
+	while (c <= 'f')
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
