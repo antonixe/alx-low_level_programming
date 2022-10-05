23fb9f554b19850ea33cbc33d154b8f6b76a24f8
antonixe <khaaoyatoony@gmail.com>
2022-10-05 12:06:01 -0700

    task0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,6 +5,7 @@
  * main- a program that prints its name
  * @argc: holds the number of arguments passed
  * @argv: array pointer that holds the arguments passed
+ * Return: Always 0 (Success)
  */
 
 int main(int argc, char *argv[])
