ac6914db86db0fe7ba2d39c3eae60228478496af
antonixe <khaaoyatoony@gmail.com>
2022-12-17 05:43:00 -0800

    0x18

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,12 +1,6 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-
-
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -28,4 +22,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif /*_MAIN_H_*/
+#endif /* _MAIN_H_ */
